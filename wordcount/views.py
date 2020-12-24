0685b37a9be0118f6938c4083ed20095dae7a3e4
Warren <wjbartholomew@hotmail.com>
2020-12-24 14:33:15 -0600

    functional word counter

---
+++ wordcount/views.py
@@ -0,0 +1,28 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+import operator
+
+
+def homepage(request):
+    return render(request, 'home.html')
+
+
+def count(request):
+    fulltext = request.GET['fulltext']
+    wordlist = fulltext.split()
+
+    wordcountdict = {}
+
+    for word in wordlist:
+        count
+        if word in wordcountdict:
+            # increment counter
+            wordcountdict[word] += 1
+        else:
+            # add word to dictionary
+            wordcountdict[word] = 1
+
+    sorteddict = sorted(wordcountdict.items(),
+                        key=operator.itemgetter(1), reverse=True)
+
+    return render(request, 'count.html', {'fulltext': fulltext, 'count': len(wordlist), 'sorteddict': sorteddict})
